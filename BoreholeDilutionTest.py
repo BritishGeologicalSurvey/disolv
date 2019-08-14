77c19b595efcba992e81c2ab09be32e74d1dd00a
John A Stevenson <jostev@bgs.ac.uk>
2019-08-14 13:54:06 +0100

    Initial commit of code files

---
+++ BoreholeDilutionTest.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  7 13:22:42 2018
+
+@author: sarcol
+"""
+
+import os
+import numpy as np
+import SolveEquation
+import matplotlib.pyplot as plt
+
+def ConvertFEC(InData,Temp):
+    
+    FEC20 = (1 + 0.024 * (Temp - 20)) * InData
+    Concentrations = (1870 - np.sqrt(1870**2 - 80 * FEC20))/80
+
+    return Concentrations    
+
+
+#---------------------Get input parameters-----------------------
+
+Parameters = np.genfromtxt("Input\\in.dat",skip_footer=1)
+
+length = Parameters[0]
+z = Parameters[1]
+A = Parameters[2]
+alpha = Parameters[3]
+Cc = Parameters[4]
+FECconvert = Parameters[5]
+Temp = Parameters[6]
+Calibrate = Parameters[7]
+
+Bounds = np.genfromtxt("Input\\in.dat",skip_header=16,skip_footer=1)
+t = np.genfromtxt("Input\\in.dat",skip_header=18)
+t = np.concatenate((np.array([0]),t))
+
+#----------------------------Grid---------------------------------
+
+N_nodes = int(round((length+z)/z))
+x = np.linspace(0,length,N_nodes)
+
+#---------Initial condition and Observations-----------------------
+
+in_con_raw = np.genfromtxt('Input\\initialcondition.dat',skip_header=1)
+ObsProfilesRaw = np.genfromtxt("Input\\measuredprofiles.dat",skip_header=1)
+
+x = np.linspace(0,length,N_nodes)
+
+NObs = int(np.shape(ObsProfilesRaw)[1]/2)
+ObservedProfiles = np.zeros([len(x),NObs])
+
+if Parameters[5] == 2:
+    Concentrations = ConvertFEC(in_con_raw[:,1],Temp)
+    for i in range(NObs):
+        ObsProfilesRaw[:,2*i + 1] = ConvertFEC(ObsProfilesRaw[:,2*i + 1],Temp)
+else:
+    Concentrations = in_con_raw[:,1]
+
+in_con = np.interp(x,in_con_raw[:,0],Concentrations)
+
+#Oberservation x points for automatic calibration
+
+for i in range(NObs):
+    ObservedProfiles[:,i] = np.interp(x,ObsProfilesRaw[:,2*i],ObsProfilesRaw[:,2*i + 1])
+
+
+
+#------------------------Make sure Qin = Qout---------------------
+
+indata = np.genfromtxt("Input\\flows.dat",skip_header=1)[:,:2]
+
+Nflows = np.shape(indata)[0]
+
+for i in range(Nflows):
+    
+    indata[i,0] = round(indata[i,0],1)
+
+inflows = np.zeros([Nflows,2])
+outflows = np.zeros([Nflows,2])
+
+for i in range(Nflows):
+    
+    if indata[i,1] < 0:
+        
+        outflows[i,0] = indata[i,0]
+        outflows[i,1] = -indata[i,1]
+    
+    if indata[i,1] > 0:
+        
+        inflows[i,0] = indata[i,0]
+        inflows[i,1] = indata[i,1]
+        
+ 
+#-------------------Call function that solves equation----------------
+if Calibrate == 0:
+    sim_profiles = SolveEquation.forward(N_nodes,inflows,outflows,z,alpha,Cc,Nflows,in_con,t,A)
+
+
+#-------------------------Automatic calibration------------------------
+ 
+if Calibrate == 1:
+    
+    output = SolveEquation.inverse(N_nodes,inflows,outflows,z,alpha,Cc,Nflows,in_con,t,A,ObservedProfiles,Bounds)
+    
+    alpha = output.x[0]
+    inflows[:,1] = output.x[1:(Nflows+1)]/np.sum(output.x[1:(Nflows+1)]) * output.x[-1]
+    outflows[:,1] = output.x[(Nflows+1):-1]/np.sum(output.x[(Nflows+1):-1])* output.x[-1]
+    
+    sim_profiles = SolveEquation.forward(N_nodes,inflows,outflows,z,alpha,Cc,Nflows,in_con,t,A)
+    
+             
+    
+out_values = sim_profiles[1:,:]
+OutStr = []
+for i in range(len(t)-1):
+    OutStr.append(str(t[i+1])+'\t')
+OutStr.append('\n')
+for j in range(N_nodes):
+    for i in range(len(t)-1):
+        OutStr.append(str(out_values[i,j]) + '\t')
+    OutStr.append('\n')
+
+f = open('Output\\profiles.out', 'w')
+for item in OutStr:
+	f.write("%s" % item)
+f.close()
+
+#-----------------Plot results------------------
+
+plt.figure()
+for i in range(NObs):
+    plt.plot(sim_profiles[i+1,:],x,label=str(t[i+1]))
+    plt.scatter(ObsProfilesRaw[:,2*i + 1],ObsProfilesRaw[:,2*i])
+
+plt.legend()
+plt.xlabel('Salinity (kg/m$^3$)')
+plt.ylabel('Depth below ground (m)')
+plt.gca().invert_yaxis()
+plt.savefig(os.path.join('Output', 'result.png'))
---
+++ SolveEquation.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 11 09:12:32 2019
+
+@author: sarcol
+"""
+
+import numpy as np
+from scipy.integrate import odeint
+from scipy.sparse import diags
+from scipy.optimize import minimize
+import sys
+
+
+def WriteMatrix(N_nodes,inflows,outflows,z,alpha,Cc,Nflows):
+    
+        
+    if abs(np.sum(inflows[:,1]) - np.sum(outflows[:,1]))/np.sum(inflows[:,1]) > 0.001:
+        
+        
+        sys.exit("Inflow does not equal outflow")
+        
+
+
+    #---------Put inflows and outflows in arrays------------
+      
+    qout = np.zeros(N_nodes)
+    qin = np.zeros(N_nodes)
+    
+    for i in range(Nflows):
+        
+        nodein = int(inflows[i,0]/z)
+        nodeout = int(outflows[i,0]/z)
+        
+        qin[nodein] = inflows[i,1]
+        qout[nodeout] = outflows[i,1]
+        
+    
+    #--------------Calculate flows at every node------------
+    
+    Qculm = 0
+    Flow_front = np.zeros(N_nodes)
+    Flow_back = np.zeros(N_nodes)
+    
+    for i in range(N_nodes-1):
+        Flow = Qculm + (qin[i] - qout[i])
+        Flow_front[i] = Flow
+        Flow_back[i+1] = Flow
+        Qculm = Flow
+     
+        
+    Flow_front = Flow_front * z
+    Flow_back = Flow_back * z
+        
+    Dfront = abs(Flow_front * alpha)
+    Dback = abs(Flow_back * alpha)
+    
+
+    
+    #-------------------Write function---------------------   
+      
+    p1 = Dfront + Dback
+    p2 = Dfront
+    p3 = Dback
+    p4 = Flow_front
+    p5 = Flow_back
+    p6 = qin * Cc
+    p7 = qout
+    
+    Eqs = np.zeros([N_nodes,3])
+    
+    for i in range(N_nodes - 2):
+        
+        j = i + 1
+        
+        #In order ci , ci+1, ci-1
+        Eqs[j,:] = np.array([-p1[j]/z**2 - p4[j]/(2*z) + p5[j]/(2*z) - p7[j], p2[j]/z**2 - p4[j]/(2*z), p3[j]/z**2 + p5[j]/(2*z)])
+    
+    #First and last lines
+    Eqs[0,:] = [-p1[0]/z**2 - p4[0]/(2*z) + p5[0]/(2*z) - p7[0], (p2[0]+p3[0])/z**2 - (p4[0] - p5[0])/(2*z),0]
+    Eqs[-1,:] = [-p1[-1]/z**2 - p4[-1]/(2*z) + p5[-1]/(2*z) - p7[-1],0, (p2[-1]+p3[-1])/z**2 - (p4[-1] - p5[-1])/(2*z)]
+    
+    
+    #Eqs are in order ci , ci+1, ci-1
+    Matrix = diags([Eqs[1:,2],Eqs[:,0],Eqs[:-1,1]],[-1,0,1],shape=(N_nodes,N_nodes)).todense()
+    
+    return Matrix, p6
+
+def forward(N_nodes,inflows,outflows,z,alpha,Cc,Nflows,in_con,t,A):
+    
+    Matrix, p6 = WriteMatrix(N_nodes,inflows,outflows,z,alpha,Cc,Nflows)[0],WriteMatrix(N_nodes,inflows,outflows,z,alpha,Cc,Nflows)[1]
+            
+    #Make function
+    def dX_dt(sm, t):
+        return 1/A * np.squeeze(np.asarray(np.dot(Matrix,sm)  + p6))
+    
+    #Solver
+    wsol = odeint(dX_dt, in_con, t, atol=1e-08, rtol=1e-06,mxstep=5000000)
+    
+    return wsol
+
+def inverse(N_nodes,inflows,outflows,z,alpha,Cc,Nflows,in_con,t,A,Obs,Bound):
+
+    def CalculateRMSE(invec):
+        
+                
+        alpha = invec[0]
+        variable_inflow = invec[1:int(1 + Nflows)]
+        variable_outflow = invec[int(1 + Nflows):-1]
+        
+        inflows[:,1] =  variable_inflow/np.sum(variable_inflow) * invec[-1]
+        outflows[:,1] = variable_outflow/np.sum(variable_outflow) * invec[-1]
+                       
+                
+        Matrix, p6 = WriteMatrix(N_nodes,inflows,outflows,z,alpha,Cc,Nflows)[0],WriteMatrix(N_nodes,inflows,outflows,z,alpha,Cc,Nflows)[1]
+            
+        #Make function
+        def dX_dt(sm, t):
+            return 1/A * np.squeeze(np.asarray(np.dot(Matrix,sm)  + p6))
+        
+        #Solver
+        wsol = odeint(dX_dt, in_con, t, atol=1e-08, rtol=1e-06,mxstep=5000000)
+        
+        #Calculate root mean square error    
+        r = np.sqrt(np.nanmean(((wsol[1:,:].transpose() - Obs)**2)))
+        
+        
+        return r
+    
+    #Need to make sure the optimization preserves total inflow = total outflows
+    
+    inflow_frac = inflows[:,1]/np.sum(inflows[:,1])
+    outflow_frac = outflows[:,1]/np.sum(outflows[:,1])
+    
+    Ini_tot_flow = np.sum(inflows[:,1])
+    
+    #create parameter array input
+    param = [alpha] + inflow_frac.tolist() + outflow_frac.tolist() + [Ini_tot_flow]  
+    #create bounds input
+    a = ()
+    b = ()
+    for i in range(Nflows):
+        if inflows[i,0] == 0:
+            a = a + ((0,0),)
+        else:
+            a = a + ((0.01,1),)   
+        if outflows[i,0] == 0:
+            b = b + ((0,0),)
+        else:
+            b = b + ((0.01,1),)
+            
+    
+    parambounds = ((Bound[0],Bound[1]),) + a + b + ((Bound[2],Bound[3]),)
+     
+    return minimize(CalculateRMSE,param,method='L-BFGS-B',bounds=parambounds)
+
+
+
+
+
+
+
