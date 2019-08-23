cabf148ab108dad3d4ef1b0da4a35d5b7e8d812c
Collins <sarcol@bgs.ac.uk>
2019-08-23 14:46:54 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='disolv',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Sarah Collins',
+      author_email='sarcol@bgs.ac.uk',
+      install_requires=['numpy','scipy','matplotlib','pandas' ],
+     )
