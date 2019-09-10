a1edff56dd09b5c758eb3369c7a947019009ad12
Collins <sarcol@bgs.ac.uk>
2019-09-10 08:49:55 +0100

    setup.py changes

--- setup.py
+++ setup.py
@@ -7,8 +7,9 @@ Setup script for the disolv package
 from distutils.core import setup
 
 setup(name='disolv',
-      version='1.0',
-      description='Python Distribution Utilities',
+      version='1.2',
+	  licence='GNU LGPLv3'
+      description='Modelling dilution tests',
       author='Sarah Collins',
       author_email='sarcol@bgs.ac.uk',
       url='https://github.com/sarcol-bgs/disolv/',
