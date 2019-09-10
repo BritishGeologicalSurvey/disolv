3751b3289ae374d825881bd6b60c87f5836e5ac0
Collins <sarcol@bgs.ac.uk>
2019-09-10 08:59:50 +0100

    setup.py correction

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 
 setup(name='disolv',
       version='1.2',
-	  licence='GNU LGPLv3',
+	  license='GNU LGPLv3',
       description='Modelling dilution tests',
       author='Sarah Collins',
       author_email='sarcol@bgs.ac.uk',
