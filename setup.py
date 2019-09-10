de55a7d26007d24460f1f875086578289f26c38e
sarcol-bgs <54437682+sarcol-bgs@users.noreply.github.com>
2019-09-10 08:17:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(name='disolv',
       description='Python Distribution Utilities',
       author='Sarah Collins',
       author_email='sarcol@bgs.ac.uk',
+      url='https://github.com/sarcol-bgs/disolv/',
       packages=["disolv"],
       install_requires=['numpy', 'scipy', 'matplotlib', 'pandas'],
       )
